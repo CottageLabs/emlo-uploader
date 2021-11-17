132a53ea29969ef86a09234a2a06ae6749234ef2
Hrafn Malmquist <hrafn.malmquist@gmail.com>
2021-11-17 00:13:32 +0000

    Refactoring done

--- main.py
+++ main.py
@@ -12,6 +12,75 @@ LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO, format=LOG_FORMAT)
 
 
+def preprocess_languages(r_surplus, upload_id, iwork_id):
+    work_languages = []
+
+    if r_surplus['hashebrew']:
+        work_languages.append("heb")
+
+    if r_surplus['hasarabic']:
+        work_languages.append("ara")
+
+    if r_surplus['hasgreek']:
+        work_languages.append("ell")
+
+    if r_surplus['haslatin']:
+        work_languages.append("lat")
+
+    if len(work_languages):
+        process_languages(work_languages, upload_id, iwork_id)
+
+
+def process_authors(author_ids, author_names, upload_id, iwork_id):
+    authors = process_people(str(author_ids),
+                             str(author_names),
+                             upload_id)
+
+    author_id = session.query(db.CofkCollectAuthorOfWork.author_id) \
+        .order_by(desc(db.CofkCollectAuthorOfWork.author_id)).first()
+
+    if not author_id:
+        author_id = 1
+    else:
+        author_id = author_id[0]
+
+    for p in authors:
+        author = db.CofkCollectAuthorOfWork(
+            author_id=author_id,
+            upload_id=upload_id,
+            iwork_id=iwork_id,
+            iperson_id=p)
+        author_id = author_id + 1
+
+        session.add(author)
+    session.commit()
+
+
+def process_addressees(addressee_ids, addressee_names, upload_id, iwork_id):
+    addressees = process_people(str(addressee_ids),
+                                str(addressee_names),
+                                upload_id)
+
+    addressee_id = session.query(db.CofkCollectAddresseeOfWork.addressee_id) \
+        .order_by(desc(db.CofkCollectAddresseeOfWork.addressee_id)).first()
+
+    if not addressee_id:
+        addressee_id = 1
+    else:
+        addressee_id = addressee_id[0]
+
+    for p in addressees:
+        addressee = db.CofkCollectAddresseeOfWork(
+            addressee_id=addressee_id,
+            upload_id=upload_id,
+            iwork_id=iwork_id,
+            iperson_id=p)
+        addressee_id = addressee_id + 1
+
+        session.add(addressee)
+    session.commit()
+
+
 def process_row(r_insert):
     # Establish what keys are surplus to table
     r_missing = list(set(r_insert.keys()) - set([c for c in CofkCollectWork.__dict__.keys()]))
@@ -20,10 +89,6 @@ def process_row(r_insert):
     upl = CofkCollectUpload(upload_username='cofka')
     session.add(upl)
     session.commit()
-
-    people_mentioned = process_people(str(r_insert['emlo_mention_id']),
-                                      str(r_insert['mention_id']),
-                                      upl.upload_id)
 
     r_surplus = {}
     # Deleting surplus keys
@@ -51,6 +116,27 @@ def process_row(r_insert):
     session.add(w)
     session.commit()
 
+    process_mentions(r_surplus['emlo_mention_id'], r_surplus['mention_id'],
+                     upl.upload_id, w.iwork_id)
+
+    preprocess_languages(r_surplus, upl.upload_id, w.iwork_id)
+
+    if r_surplus['resource_name'] or r_surplus['resource_url']:
+        process_resource(upl.upload_id, w.iwork_id, r_surplus['resource_name'],
+                         r_surplus['resource_url'], r_surplus['resource_details'])
+
+    process_authors(r_surplus['author_ids'], r_surplus['author_names'],
+                    upl.upload_id, w.iwork_id)
+
+    process_addressees(r_surplus['addressee_ids'], r_surplus['addressee_names'],
+                       upl.upload_id, w.iwork_id)
+
+
+def process_mentions(emlo_mention_ids, mention_ids, upload_id, iwork_id):
+    people_mentioned = process_people(str(emlo_mention_ids),
+                                      str(mention_ids),
+                                      upload_id)
+
     mention_id = session.query(CofkCollectPersonMentionedInWork.mention_id) \
         .order_by(desc(CofkCollectPersonMentionedInWork.mention_id)).first()
 
@@ -62,79 +148,12 @@ def process_row(r_insert):
     for p in people_mentioned:
         person_mention = CofkCollectPersonMentionedInWork(
             mention_id=mention_id,
-            upload_id=upl.upload_id,
-            iwork_id=w.iwork_id,
+            upload_id=upload_id,
+            iwork_id=iwork_id,
             iperson_id=p)
         mention_id = mention_id + 1
 
         session.add(person_mention)
-    session.commit()
-
-    work_languages = []
-
-    if r_surplus['hashebrew']:
-        work_languages.append("heb")
-
-    if r_surplus['hasarabic']:
-        work_languages.append("ara")
-
-    if r_surplus['hasgreek']:
-        work_languages.append("ell")
-
-    if r_surplus['haslatin']:
-        work_languages.append("lat")
-
-    if len(work_languages):
-        process_languages(work_languages, upl.upload_id, w.iwork_id)
-
-    if r_surplus['resource_name'] or r_surplus['resource_url']:
-        process_resource(upl.upload_id, w.iwork_id, r_surplus['resource_name'],
-                         r_surplus['resource_url'], r_surplus['resource_details'])
-
-    authors = process_people(str(r_surplus['author_ids']),
-                             str(r_surplus['author_names']),
-                             upl.upload_id)
-
-    author_id = session.query(db.CofkCollectAuthorOfWork.author_id) \
-        .order_by(desc(db.CofkCollectAuthorOfWork.author_id)).first()
-
-    if not author_id:
-        author_id = 1
-    else:
-        author_id = author_id[0]
-
-    for p in authors:
-        author = db.CofkCollectAuthorOfWork(
-            author_id=author_id,
-            upload_id=upl.upload_id,
-            iwork_id=w.iwork_id,
-            iperson_id=p)
-        author_id = author_id + 1
-
-        session.add(author)
-    session.commit()
-
-    addressees = process_people(str(r_surplus['addressee_ids']),
-                             str(r_surplus['addressee_names']),
-                             upl.upload_id)
-
-    addressee_id = session.query(db.CofkCollectAddresseeOfWork.addressee_id) \
-        .order_by(desc(db.CofkCollectAddresseeOfWork.addressee_id)).first()
-
-    if not addressee_id:
-        addressee_id = 1
-    else:
-        addressee_id = addressee_id[0]
-
-    for p in addressees:
-        addressee = db.CofkCollectAddresseeOfWork(
-            addressee_id=addressee_id,
-            upload_id=upl.upload_id,
-            iwork_id=w.iwork_id,
-            iperson_id=p)
-        addressee_id = addressee_id + 1
-
-        session.add(addressee)
     session.commit()
 
 
